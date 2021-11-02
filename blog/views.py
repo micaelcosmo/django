1642a02111240e340d121a2416c3ea5de5600804
Micael Cosmo <micael.trabalho@hotmail.com>
2021-11-02 14:35:38 -0300

    Update views.py Criado views de lista. Essa view obterá todas as postagens cujo status seja ''published'' usando o gerenciador ''published''.(em models.py)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts':posts})
