580eb04c9113cce9aeffeb38a95de9f38bd1a72d
Micael Cosmo <micael.trabalho@hotmail.com>
2021-11-02 14:15:02 -0300

    Update models.py Criado gerenciadores de modelo default e personalizado.

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -22,6 +28,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
