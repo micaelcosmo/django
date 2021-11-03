a956ed71a4b8f0b43b4450967d0af2374ffcdcd6
Micael Cosmo <micael.trabalho@hotmail.com>
2021-11-03 13:00:47 -0300

    Update views.py Criado views de detalhes da postagem. Essa view recebe os argumentos "year, month, day e post" para obter uma postagem publicada com o slug e a data especificados.

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,15 @@ def post_list(request):
     posts = Post.published.all()
     return render(request,
                   'blog/post/list.html',
-                  {'posts':posts})
+                  {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
