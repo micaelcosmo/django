718e75659e63eb4cf5c086aa15a1918b660d4bb4
Micael Cosmo <micael.trabalho@hotmail.com>
2021-11-04 13:29:37 -0300

    Update models.py Adicionado de acordo com a convenção de Django, o método ''get_absolute_url()" que retorna URL canônico para o objeto.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -36,3 +37,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+def get_absolute_url(self):
+    return reverse('blog:post_detail',
+                   args=[self.publish.year,
+                         self.publish.month,
+                         self.publish.day, self.slug])
