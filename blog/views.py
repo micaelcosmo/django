81f8c9555378bcf1330e12ee5783272b98460d17
Micael Cosmo <micael.trabalho@hotmail.com>
2021-11-04 21:25:34 -0300

    Update views.py Adicionado importação da classe ListView. Adicionado view baseada em classe que é análocga à view post_list, assim utiliza um QuerySet específico em vez de obter todos os objetos.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
 # Create your views here.
@@ -28,3 +29,10 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
