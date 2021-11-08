dbe53075faf735e0752bc668396ce90629bced1e
Micael Cosmo <micael.trabalho@hotmail.com>
2021-11-08 10:04:17 -0300

    Update views.py Importado a classe "EmailPostForm" Adicionado view que recebe o objeto "request" e a variável "post_id" como parâmetros, usando o atalho "get_object_or_404" para obter a postagem com base no Id e garantir que a postagem obtida tenha um status igual a "published". A view será usada para exibir tanto o formulário inicial como para processar os dados submetidos, diferenciando se o formulário foi submetido ou não com base no método de "request" e submetendo o formulário com "POST".

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
 # Create your views here.
@@ -29,6 +30,18 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
 
 
 class PostListView(ListView):
