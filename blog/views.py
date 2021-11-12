1c717ab2d22ac0461cf42670fa5a60b5fb133a75
Micael Cosmo <micael.trabalho@hotmail.com>
2021-11-12 10:04:22 -0300

    Update views.py Adicionado variável "sent" que é definida como "True" quando a postagem é enviada, a fim de ser usada em template para exibir mensagem de sucesso se o formulário for submetido com sucesso. Adicionado forma de obter o path absoluto da postagem usando o método "get_absolute_url()", utilizando esse path como entrada para "request.build_aboslute_uri()" a fim de compor um URL completo, incluindo o esquema HTTP e o nome do host. Definido o assunto e o corpo da mensagem do email usando os dados limpos do formulário validado, que por fim envia o email para o endereço contido no campo "to" do formulário.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -34,14 +35,26 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} remmends you read "\
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'EmailBackend',
+                      [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form})
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 class PostListView(ListView):
