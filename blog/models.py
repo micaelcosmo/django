2295f3b787c9c2f48efbc390c90c0ebc6ade247d
Micael Cosmo <micael.trabalho@hotmail.com>
2022-01-14 18:14:19 -0300

    Update models.py Correção ortográfica.

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class PublishedManager(models.Manager):
                      self).get_queryset()\
                           .filter(status='published')
 
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -50,7 +51,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,
                              on_delete=models.CASCADE,
                              related_name='comments')
-    name = models.CharField(max_lenght=80)
+    name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
