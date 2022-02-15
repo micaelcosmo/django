8b378c157a4f60f3ffa6a7ceb8049178ced0eebe
Micael Cosmo <micael.trabalho@hotmail.com>
2022-02-15 12:07:28 -0300

    Update models.py Adicionado gerenciador TaggableManager disponibilizado pelo django-taggit no modelo Post.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
@@ -32,6 +33,7 @@ class Post(models.Model):
                               default='draft')
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
