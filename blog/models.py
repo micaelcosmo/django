f4f31440ff399a3db3e7ea648960544dd2b078cb
Micael Cosmo <micael.trabalho@hotmail.com>
2022-02-15 13:11:40 -0300

    Update models.py Alocado função get_absolute_url para dentro da classe Post, fazendo com que abra uma nova janela mostrando detalhes da postagem.

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,8 @@ class PublishedManager(models.Manager):
                           .filter(status='published')
 
 
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -35,18 +37,17 @@ class Post(models.Model):
     published = PublishedManager()
     tags = TaggableManager()
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
-
-
-def get_absolute_url(self):
-    return reverse('blog:post_detail',
-                   args=[self.publish.year,
-                         self.publish.month,
-                         self.publish.day, self.slug])
 
 
 class Comment(models.Model):
